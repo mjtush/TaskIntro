15c4cca3125e9bcd5481cfd3872eedcc8a52410b
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-14 18:20:10 +0200

    Add project files.

---
+++ TaskIntro/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TaskIntro
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var task = new Task(SimpleMethod);
+            task.Start();
+            task.Wait();
+
+            var taskThatReturns = new Task<string>(MethodThatReturns);
+            taskThatReturns.Start();
+            taskThatReturns.Wait();
+            Console.WriteLine(taskThatReturns.Result);
+        }
+
+        private static string MethodThatReturns()
+        {
+            Thread.Sleep(2000);
+            return "Hello from return method!";
+        }
+
+        private static void SimpleMethod()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
