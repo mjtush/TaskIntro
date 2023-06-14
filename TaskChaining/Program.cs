8ef9a4482cfebe6cdc6559f91f5b82f239119d8f
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-06-14 20:04:04 +0200

    Task chaining demo

---
+++ TaskChaining/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using System;
+using System.Threading.Tasks;
+
+namespace TaskChaining
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Task<string> antecedent = Task.Run(() =>
+            {
+                Task.Delay(TimeSpan.FromSeconds(2));
+                return DateTime.Today.ToShortDateString();
+            });
+
+            Task<string> continuation = antecedent.ContinueWith(t => "Today is "
+                + antecedent.Result);
+#if DEBUG
+            //Debug.WriteLine("This will display before the result");
+            Console.WriteLine("This will display before the result");
+#endif
+            Console.WriteLine(continuation.Result);
+        }
+    }
+}
